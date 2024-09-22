49791f77088943dc306ed3bdd4cd975f05594abf
Gosia Bak <machmalgorzata@gmail.com>
2024-09-22 22:30:42 +0200

    Add rabbit mq service bus

--- AuctionService/Program.cs
+++ AuctionService/Program.cs
@@ -1,4 +1,6 @@
+using AuctionService.Consumers;
 using AuctionService.Data;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,9 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 builder.Services.AddDbContext<AuctionDbContext>(opt =>
 {
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+
 });
 
 
@@ -18,10 +23,25 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
 //builder.Services.AddSwaggerGen();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddMassTransit(x =>
+{
+    x.AddEntityFrameworkOutbox<AuctionDbContext>(
+        o =>
+        {
+            o.QueryDelay = TimeSpan.FromSeconds(10);
+            o.UsePostgres();
+            o.UseBusOutbox();
+        });
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
+    x.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.ConfigureEndpoints(ctx);
+    });
+});
+
 
 var app = builder.Build();
-
-
 
 //COMMENTED JUST FOR NOW AS THE SWAGGER MIGHT NOT BE USED AT ALL
 // Configure the HTTP request pipeline.
@@ -30,8 +50,6 @@ var app = builder.Build();
 //    app.UseSwagger();
 //    app.UseSwaggerUI();
 //}
-
-
 
 //BYC MOE KONIECZNE BEDZIE ZAKOMENTOWANIE PONIEWAZ NIE BEDZIE UZYWANE HTTP ??????
 app.UseHttpsRedirection();
