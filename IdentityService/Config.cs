111e1fa993483401f55627c6df26d9eaa9d501d9
Gosia Bak <machmalgorzata@gmail.com>
2024-09-29 21:27:52 +0200

    #Create IdentityService with basic registration

---
+++ IdentityService/Config.cs
@@ -0,0 +1,34 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityService
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("auctionApp", "Auction App Full Access"),
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                    {
+                        ClientId = "postman",
+                        ClientName = "Postman",
+                        AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                        ClientSecrets = { new Secret("NotASecret".Sha256()) },
+                        AllowedScopes = { "openid", "profile", "auctionApp" },
+                        RedirectUris = {"https://www.getpostman.com/auth2/callback"}
+                    },
+            };
+    }
+}
